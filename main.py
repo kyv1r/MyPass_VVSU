db82a10f83dad3ad499ba83f37bc68d9f7ace1f1
Artorius81 <91239017+Artorius81@users.noreply.github.com>
2021-10-01 17:32:01 +1000

    Add KivyMD. New files for login form.

--- main.py
+++ main.py
@@ -1,71 +1,46 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.lang.builder import Builder
+from kivymd.app import MDApp
 
-class MainApp(App):
+kv = """
+<MySwiper@MDSwiperItem>
+
+    canvas.before:
+        Color:
+            rgba: app.theme_cls.bg_darkest
+        RoundedRectangle:
+            size: self.size
+            pos: self.pos
+
+    MDLabel:
+        text: 'MDSwiperItem'
+        halign: 'center'
+
+MDBoxLayout:
+    orientation: 'vertical'
+    spacing: '8dp'
+
+    MDToolbar:
+        id: toolbar
+        title: 'MDSwiper'
+        elevation: 10
+
+    MDSwiper:
+
+        MySwiper:
+
+        MySwiper:
+
+        MySwiper:
+
+        MySwiper:
+
+        MySwiper:
+"""
+
+
+class Example(MDApp):
     def build(self):
-        self.operators = ["/", "*", "+", "-"]
-        self.last_was_operator = None
-        self.last_button = None
-        main_layout = BoxLayout(orientation="vertical")
-        self.solution = TextInput(
-            multiline=False, readonly=True, halign="right", font_size=55
-        )
-        main_layout.add_widget(self.solution)
-        buttons = [
-            ["7", "8", "9", "/"],
-            ["4", "5", "6", "*"],
-            ["1", "2", "3", "-"],
-            [".", "0", "C", "+"],
-        ]
-        for row in buttons:
-            h_layout = BoxLayout()
-            for label in row:
-                button = Button(
-                    text=label,
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
-
-        equals_button = Button(
-            text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
-        )
-        equals_button.bind(on_press=self.on_solution)
-        main_layout.add_widget(equals_button)
-
-        return main_layout
-
-    def on_button_press(self, instance):
-        current = self.solution.text
-        button_text = instance.text
-
-        if button_text == "C":
-            # Очистка виджета с решением
-            self.solution.text = ""
-        else:
-            if current and (
-                self.last_was_operator and button_text in self.operators):
-                # Не добавляйте два оператора подряд, рядом друг с другом
-                return
-            elif current == "" and button_text in self.operators:
-                # Первый символ не может быть оператором
-                return
-            else:
-                new_text = current + button_text
-                self.solution.text = new_text
-        self.last_button = button_text
-        self.last_was_operator = self.last_button in self.operators
-
-    def on_solution(self, instance):
-        text = self.solution.text
-        if text:
-            solution = str(eval(self.solution.text))
-            self.solution.text = solution
+        return Builder.load_string(kv)
 
 
-if __name__ == "__main__":
-    app = MainApp()
-    app.run()
+Example().run()
